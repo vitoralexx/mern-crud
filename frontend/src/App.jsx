2cce3540dba7a869163de76c28de6c5e004ad6d7
vitoralexx <vitoralexse@gmail.com>
2025-10-02 22:47:19 -0300

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  return (
+    <>
+      <Toaster position="top-right" />
+      <BrowserRouter>
+        <Routes>
+
+          {/*  */}
+          <Route path={} element={ }>
+
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
